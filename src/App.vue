45f8fd9c51f3c8392a5992fed426732873c4f771
Brice <lebowvsky@gmail.com>
2022-11-06 00:40:40 +0100

    feat: :sparkles: Same behaviour with custom checkboxes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
   <h1>DSFR checkbox</h1>
-  <DsfrCheckbox
-    label="ALL"
-    name="all"
-    @change="($event) => lord(allChecked)"
-    v-model="allChecked"
-  />
   <div class="container">
-    <template v-for="(data, index) in datas" :key="data">
-      <DsfrCheckbox
-        :label="data"
-        :value="data"
-        :name="data"
-        :checked="isChecked[index]"
-        @change="($event) => youpi($event.target.value, index)"
-        v-model="isChecked[index]"
-      />
-    </template>
-    <p>{{modelValue}}</p>
-    <p>{{isChecked}}</p>
-    <p>{{allChecked}}</p>
+    <section class="dsfr-section">
+      <DsfrCheckbox label="ALL" name="all" @change="lord(allChecked)" v-model="allChecked" />
+      <template v-for="(data, index) in datas" :key="data">
+        <DsfrCheckbox
+          :label="data"
+          :value="data"
+          :name="data"
+          :checked="isChecked[index]"
+          @change="($event) => youpi($event.target.value, index)"
+          v-model="isChecked[index]"
+        />
+      </template>
+      <p>{{ modelValue }}</p>
+      <p>{{ isChecked }}</p>
+      <p>{{ allChecked }}</p>
+    </section>
+    <section class="custom-section">
+      <DsfrCheckbox label="ALL" name="allcustom" v-model="allCustomChecked" @change="onAllCustomChange" />
 
+      <div class="check-wrapper" v-for="(data, index) in datas" :key="`${data}${index}`">
+        <input type="checkbox" name="youpi" id="youpi" :value="data" v-model="customResult" @change="checkboxChange" />
+      </div>
+
+      <p>{{ customResult }}</p>
+    </section>
   </div>
 </template>
 
@@ -28,30 +33,44 @@
 import { ref } from "@vue/reactivity";
 
 const modelValue = ref<any[]>([]);
-const datas = ["truc", "machin", "chose"]
+const datas = ["truc", "machin", "chose"];
 
-const isChecked = ref<any[]>([])
-const allChecked = ref(false)
+const isChecked = ref<any[]>([]);
+const allChecked = ref(false);
 
 const lord = (event: boolean) => {
   datas.forEach((data, index) => {
-    isChecked.value[index] = event
-    if(event){
-      modelValue.value[index] = data
+    isChecked.value[index] = event;
+    if (event) {
+      modelValue.value[index] = data;
     } else {
-      modelValue.value[index] = null
+      modelValue.value[index] = null;
     }
-  })
-}
+  });
+};
 
 const youpi = (event: string, index: number) => {
-  if(!isChecked.value[index] ) {
-    modelValue.value[index] = null
-    allChecked.value = false
+  if (!isChecked.value[index]) {
+    modelValue.value[index] = null;
+    allChecked.value = false;
   } else {
     modelValue.value[index] = event;
   }
-}
+};
+
+const customResult = ref<string[]>([]);
+const allCustomChecked = ref<boolean>(false);
+const onAllCustomChange = () => {
+  if (!allCustomChecked.value) {
+    customResult.value = [];
+  }
+  if (allCustomChecked.value) {
+    customResult.value = [...datas];
+  }
+};
+const checkboxChange = () => {
+  allCustomChecked.value = false;
+};
 </script>
 
 <style lang="scss">
@@ -69,8 +88,42 @@ const youpi = (event: string, index: number) => {
     display: flex;
     flex-direction: column;
 
+    section {
+      border: 1px solid grey;
+      padding: 20px;
+      margin: 30px 0;
+      border-radius: 5px;
+    }
+
     #input {
-      all: unset !important;
+      /* all: unset !important; */
+    }
+    .check-wrapper {
+      margin: 24px 0;
+
+      #youpi {
+        position: relative;
+        display: block;
+        height: 24px;
+        width: 24px;
+        border: 1px solid black;
+        opacity: 1;
+        border-radius: 3px;
+        overflow: hidden;
+
+        &:checked {
+          &::after {
+            content: "";
+            position: absolute;
+            background-color: blue;
+            background-image: url("./assets/check-line.svg");
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 24px;
+            width: 24px;
+          }
+        }
+      }
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "@gouvminint/vue-dsfr/dist/vue-dsfr.css"; // Import des styles globaux
 import VueDsfr from "@gouvminint/vue-dsfr";
+import "remixicon/fonts/remixicon.css";
 
 createApp(App).use(VueDsfr).mount("#app");
+// createApp(App).mount("#app");
